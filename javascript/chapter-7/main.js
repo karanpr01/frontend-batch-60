46b2a812a20a9512170c5b16fee64d94e2a765ea
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-05-15 09:18:25 +0530

    chapter-5-6-7

---
+++ javascript/chapter-7/main.js
@@ -0,0 +1,18 @@
+// String 
+let str1 = "Hello world i'm suraj"
+// Immutable
+
+str1[0] = "S"
+
+console.log(str1)
+
+// console.log(str1[12])
+
+// let str2 = 'This is the another way of writing strings here"s '
+
+
+// let str3 = `This is my 3rd way of writing strings ${str1} and ${function (){console.log("Hello")}}`
+
+// let str4 = "this is my str1=" + str1 +  "this is my str2="  + str2
+
+// // console.log(str4)
